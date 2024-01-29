5fc424f858838def2b335d7a4d49172c4e97a8ac
Max <wellenems13@gmail.com>
2024-01-29 13:31:12 +0100

    Anyadir pagina de inicio session registro y resetPassword

---
+++ Sesion/inicio/inicio/InicioSesion.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $correo = $_POST["Correo"];
+    $contrasenya = $_POST["Contrasenya1"];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "codebook";
+
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+
+    $query = "SELECT * FROM usuarios WHERE Correo = '$correo'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+
+        $row = $result->fetch_assoc();
+        $hashContrasenya = $row["Contraseña"];
+
+        if (password_verify($contrasenya, $hashContrasenya)) {
+
+            $_SESSION["nombreUsuario"] = $row["Nombre"];
+            header("Location: ../Code/content/HTML/content.html");
+            exit();
+        } else {
+            echo "Error: Contraseña incorrecta.";
+        }
+    } else {
+        echo "Error: Correo electrónico no encontrado.";
+    }
+
+    $conn->close();
+}
+?>
