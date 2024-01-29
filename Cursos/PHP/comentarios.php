cd02889bce5508cbe9f1fae5e0da1121dae9b6a7
David <damvidms@gmail.com>
2024-01-29 09:57:28 +0100

    cambios al php

--- Cursos/PHP/comentarios.php
+++ Cursos/PHP/comentarios.php
@@ -1,7 +1,13 @@
 <?php
+error_reporting(E_ALL);
+
+ini_set('display_errors', '1');
+
 session_start();
-$id_curso = $_GET['id_curso'];
-$nombre = $_SESSION["nombreUsuario"];
+//$id_curso = $_GET['id_curso'];
+//$nombre = $_SESSION["nombreUsuario"];
+$id_curso = 6;
+$nombre = "david";
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -12,19 +18,19 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
-$query1 = "SELECT Id FROM usuarios WHERE nombre = $nombre";
-$query = "SELECT * FROM comentarios WHERE Id_curso = $id_curso";
+
+$query = "SELECT * FROM comentarios WHERE Id_curso = '$id_curso'";
 $result = $conn->query($query);
 
 
-$episodios = array();
+$comentarios = array();
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $episodios[] = $row;
+        $comentarios[] = $row;
     }
 }
 
 $conn->close();
 
-echo json_encode($episodios);
+echo json_encode($comentarios);
 ?>
