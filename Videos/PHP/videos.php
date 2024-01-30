53a0f62781f578017b857ec0c27bd2afbcb4745e
David <davidmartinezsimon2001@gmail.com>
2024-01-30 18:24:10 +0100

    cambios estructura

--- Videos/PHP/videos.php
+++ Videos/PHP/videos.php
@@ -2,34 +2,19 @@
 
 $idEp = $_GET['id_episodio'];
 
-
 session_start();
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "codebook";
+require_once('database.php');
 
-$conn = new mysqli($servername, $username, $password, $database);
-$nombre = $_SESSION["nombreUsuario"];
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
+$database = new Database();
+
+$database->conectar();
 
 $query = "SELECT * FROM episodios WHERE Id = '$idEp'";
-$result = $conn->query($query);
+$resultadosJSON = $database->obtenerResultadoUnico($query);
 
-$datos = array();
+$database->cerrarConexion();
 
-if ($result->num_rows > 0) {
-  
-    $row = $result->fetch_assoc();
-    $datos = $row;
-    
-}
-
-$conn->close();
-
-echo json_encode($datos);
+echo $resultadosJSON;
 
 ?>
