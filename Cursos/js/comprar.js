3e7ebc769cf0e7e16cdd0e8994be0ed235f5cdcf
Max <wellenems13@gmail.com>
2024-01-31 13:39:43 +0100

    bloquea pantalla si no esta comprado el curso

--- Cursos/js/comprar.js
+++ Cursos/js/comprar.js
@@ -1,33 +1,39 @@
-fetch('../PHP/estaComprado.php?id_curso=' + idCurso2)
-  .then(response => response.json())
-  .then(data => {
+fetch("../PHP/estaComprado.php?id_curso=" + idCurso2)
+  .then((response) => response.json())
+  .then((data) => {
     console.log(data);
-    if(data == "no comprado"){
-        let divTemas = document.querySelectorAll(".content")
-        divTemas.forEach(tema => {
-            tema.style.pointerEvents = 'none';
-        });
-        divMensaje.style.pointerEvents = "none"
-    }else{
-        botonComprar.style.display = "none";
+    if (data == "no comprado") {
+      document.body.classList.toggle("difuminar");
+      let html = document.getElementsByTagName("html")[0];
+      html.classList.toggle("difuminar");
+      let divTemas = document.querySelectorAll(".content");
+      divTemas.forEach((tema) => {
+        tema.style.pointerEvents = "none";
+      });
+      divMensaje.style.pointerEvents = "none";
+    } else {
+      botonComprar.style.display = "none";
+      document.body.classList.remove("difuminar");
+      html.classList.remove("difuminar");
+      botonComprar.classList.toggle("ocultar");
     }
   })
-  .catch(error => console.error('Error:', error));
+  .catch((error) => console.error("Error:", error));
 
-  let botonComprar = document.getElementById("comprar");
+let botonComprar = document.getElementById("comprar");
 
 botonComprar.addEventListener("click", () => {
-    fetch('../PHP/comprar.php?id_curso=' + idCurso2)
-        .then(response => {
-            if (response.ok) {
-                console.log("Compra realizada con éxito");
-            } else {
-                console.error("Error en la solicitud de compra");
-            }
-            location.reload(true);
-        })
-        .catch(error => {
-            console.error("Error en la solicitud de compra: ", error);
-            location.reload(true);
-        });
+  fetch("../PHP/comprar.php?id_curso=" + idCurso2)
+    .then((response) => {
+      if (response.ok) {
+        console.log("Compra realizada con éxito");
+      } else {
+        console.error("Error en la solicitud de compra");
+      }
+      location.reload(true);
+    })
+    .catch((error) => {
+      console.error("Error en la solicitud de compra: ", error);
+      location.reload(true);
+    });
 });
