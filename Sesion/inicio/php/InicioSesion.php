2d677eeb8d95d7bfad3393d02b1843c8ab3638b3
Trierner <dario.carmona.herreras26@gmail.com>
2024-02-14 09:07:15 +0100

    Arreglo de rutas

--- Sesion/inicio/php/InicioSesion.php
+++ Sesion/inicio/php/InicioSesion.php
@@ -29,10 +29,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
          
             $_SESSION["nombreUsuario"] = $row["Nombre"];
             if ($row["Admin"] == 1) {
-                header("Location: CodeBook/AdminDashboard/AdminDashboard.html");
+                header("Location: ../../../AdminDashboard/AdminDashboard.html");
                 exit();
             } else {
-                header("Location: CodeBook/Code/content/HTML/content.html");
+                header("Location: ../../../Code/content/HTML/content.html");
                 exit();
             }
             exit();
